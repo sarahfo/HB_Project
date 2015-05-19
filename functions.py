89f1aae605c9680b33deffc546d4a0e477f22bbf
Sarah Foregger <sarahfo@Sarahs-MacBook-Pro.local>
2015-05-18 22:55:35 -0700

    Cleaned up syntax

--- functions.py
+++ functions.py
@@ -3,14 +3,37 @@ import argparse
 
 
 
-yelp_api = YelpAPI(consumer_key, consumer_secret, token, token_secret)
-search_results = yelp_api.search_query(args)
-business_results = yelp_api.business_query(id=business_id, other_args)
+# yelp_api = YelpAPI(consumer_key, consumer_secret, token, token_secret)
+# search_results = yelp_api.search_query(args)
+# business_results = yelp_api.business_query(id=business_id, other_args)
 
-def yelp_search_query(self):
-# make an empty list to hold the stuff you will pass to yelp
-	yelp=[]
-	yelp_search = self.request.get('category_filter')
+# def yelp_search_query(self):
+# # make an empty list to hold the stuff you will pass to yelp
+# 	# yelp=[]
 
-	if yelp_search:
-		# get the stuff passed from the form - if the box is checked, pass the value into yelp API.
+# 	yelp_search = request.args.getlist('category_filter')
+
+# 	print yelp_search
+
+
+def get_cat_list():
+
+	yelp_search = requests.args.getlist('category_filter')
+
+	print yelp_search
+
+
+
+
+# def yelp_business_basics(business_entry):
+#     """ http://www.yelp.com/developers/documentation/v2/search_api
+#         See Businesses structure """
+#     return {"name"          : business_entry.get("name"),
+#             "image_url"     : business_entry.get("image_url", ""),
+#             "url"           : business_entry.get("url", "None listed"),
+#             "phone"         : business_entry.get("display_phone", "None Listed"),
+#             "rating"        : business_entry.get("rating", "n/a"),
+#             "rating_image"  : business_entry.get("rating_img_url"),
+#             "review_count"  : business_entry["review_count"],
+#             "address"       : "\n".join(business_entry["location"]["display_address"])}
+	
--- server.py
+++ server.py
@@ -1,6 +1,13 @@
 """House Hunter Project"""
 
 import os
+#from functions import get_cat_list
+
+from yelpapi import YelpAPI
+import argparse
+import rauth
+import time
+import json, requests, pprint
 
 from datetime import date
 from flask import Flask, render_template, redirect, request, flash, session
@@ -18,15 +25,59 @@ app.secret_key = "ABC"
 # This is horrible. Fix this so that, instead, it raises an error.
 #app.jinja_env.undefined = StrictUndefined
 
-
 @app.route('/')
 def index():
     """Homepage."""
-    print "this runs"
+    print request.method
+    if request.args:
+    # return render_template("base.html")
+
+
+    # """Gets the list of user-selected categories and sends them to Yelp API"""
+# The above syntax should return the checked items as a list
+        yelp_search_selection = request.args.getlist('category_filter')
+
+# now make them non-unicode   
+        yelp_search = ",".join(yelp_search_selection)
+ 
+# Print Yelp Search list to confirm they aren't unicode and they look nice.
+    # data = get_results(make_search_params(yelp_search))
+        get_results(make_search_parameters(yelp_search))
+
+#I'm just doing the render template to be superstitious.
     return render_template("base.html")
 
 
+def make_search_parameters(yelp_search):
+    #Yelp API defines the key names to pass in as parameters.
+    params = {}
+    params['category_filter'] = yelp_search
+    params ["location"] = "Chicago, IL"
+    ## params ['limit'] = TBD
+    # print params
+    return params
 
+def get_results(params):
+    #OAuth Session with secret keys, sourced from OS.
+
+    auth_session = rauth.OAuth1Session(
+        consumer_key = os.environ['YELP_CONSUMER_KEY'],
+        consumer_secret = os.environ['YELP_CONSUMER_SECRET'],
+        access_token = os.environ['YELP_ACCESS_TOKEN_KEY'],
+        access_token_secret = os.environ['YELP_ACCESS_TOKEN_SECRET'])
+
+    request = auth_session.get("http://api.yelp.com/v2/search", params=params)
+    
+    #Transforms the JSON API response into a Python dictionary
+    data = json.loads(request.text)
+    #data = request.json()
+    auth_session.close()
+    pprint.pprint(data)
+    # return data
+    #print data
+
+
+# URL example for San Francisco: api.yelp.com/v2/search/?location=San Francisco, CA&limit=13&category_filter=yoga
 
 if __name__ == '__main__':
-	app.run(debug=True)
+    app.run(debug=True)
